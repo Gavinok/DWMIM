e71df0e22131b74056ce23d48f2abad6cc1a44cd
Gavinok <gavinfreeborn@gmail.com>
2019-04-04 10:36:48 -0600

    updated to a chalk theme

--- config.h
+++ config.h
@@ -11,35 +11,45 @@ static const int topbar             = 1;  /* 0 means bottom bar */
 static const char *fonts[]          = { "Siji:style=Regular:size=10" };
 static const char dmenufont[]       = "Siji:style=Regular:size=10";
 
-static const char black1[]      = "#151515";
+static const char background[]  = "#111314";
+static const char foreground[]  = "#B7BBB7";
+
+
+static const char black1[]      = "#505050";
 static const char black2[]      = "#505050";
+                                          
+                                          
 static const char red1[]		= "#fb9fb1";
-static const char red2[]		= "#fb9fb1";
+static const char red2[]		= "#B04C50";
+                                          
+                                          
 static const char green1[]      = "#acc267";
 static const char green2[]      = "#acc267";
+                                          
+                                          
 static const char yellow1[]     = "#ddb26f";
-static const char yellow2[]     = "#f0c674";
-static const char blue1[]       = "#6fc2ef";
-static const char blue2[]       = "#6fc2ef";
+static const char yellow2[]     = "#E2995C";
+                                          
+                                          
+static const char blue1[]       = "#66899D";
+static const char blue2[]       = "#66899D";
+                                          
+                                          
 static const char magenta1[]    = "#e1a3ee";
-static const char magenta2[]    = "##ff99ff";
-static const char cyan1[]       = "#12cfc0";
-static const char cyan2[]       = "#12cfc0";
+static const char magenta2[]    = "#FF99FFf";
+                                          
+                                          
+static const char cyan1[]       = "#53cdbd";
+static const char cyan2[]       = "#527C77";
+                                          
+                                          
 static const char white1[]      = "#d0d0d0";
 static const char white2[]      = "#f5f5f5";
-static const char pink[]		= "#e60053";
-static const char col_black[]   = "#000000";
-static const char col_red[]     = "#ff0000";
-static const char col_yellow[]  = "#ffff00";
-static const char col_white[]   = "#ffffff";
-static const char walbg[]   	= "#372d33";
-static const char walred[]   	= "#e36177";
-static const char walwhite[]   	= "#c3dee1";
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] =	  { white1, black1, black1 },
-	[SchemeSel]  =	  { black1, blue1,  white1 },
+	[SchemeNorm] =	  { foreground, background, background },
+	[SchemeSel]  =	  { background, cyan1,  foreground },
 };
 
 /* tagging */
