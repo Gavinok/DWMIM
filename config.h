7f0a2b45e53effe328b31b0f6598e67148f72a7c
Gavinok <gavinfreeborn@gmail.com>
2019-04-04 10:37:39 -0600

    removed the need to dmenu

--- config.h
+++ config.h
@@ -112,7 +112,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black1, "-nf", white1, "-sb", pink, "-sf", black1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 
 static const char *termcmd[]  = { "st", "-e", "fish", NULL };
 static const char scratchpadname[] = { "scratch"};
