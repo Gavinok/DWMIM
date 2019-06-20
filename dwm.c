f2e11b1a64fdb676b91f44faa1aee804305a49ad
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-06-19 22:14:31 -0600

    remo

--- dwm.c
+++ dwm.c
@@ -51,7 +51,6 @@
 #define ENABLEUSLESSGAPS
 #define ENABLECENTERWINDOWNAME
 /* #define INVERTSTATUSCOLORS */
-/* #define ENABLETILEGAPS */
 /* #define ENABLESTATUSCOLORS */
 
 /* macros */
@@ -1819,39 +1818,7 @@ tagmon(const Arg *arg)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
 }
-#ifdef ENABLETILEGAPS
-void
-tile(Monitor *m)
-{
-	unsigned int i, n, h, mw, my, ty, ns;
-	Client *c;
 
-	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if (n == 0)
-		return;
-
-	if (n > m->nmaster) {
-		mw = m->nmaster ? m->ww * m->mfact : 0;
-
-		ns = m->nmaster > 0 ? 2 : 1;
-	} else {
-		mw = m->ww;
-		ns = 1;
-	}
-	for(i = 0, my = ty = gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
-		if (i < m->nmaster) {
-
-			h = (m->wh - my) / (MIN(n, m->nmaster) - i) - gappx;
-			resize(c, m->wx + gappx, m->wy + my, mw - (2*c->bw) - gappx*(5-ns)/2, h - (2*c->bw), False);
-			my += HEIGHT(c) + gappx;
-		} else {
-
-			h = (m->wh - ty) / (n - i) - gappx;
-			resize(c, m->wx + mw + gappx/ns, m->wy + ty, m->ww - mw - (2*c->bw) - gappx*(5-ns)/2, h - (2*c->bw), False);
-			ty += HEIGHT(c) + gappx;
-		}
-}
-#else
 void
 tile(Monitor *m)
 {
@@ -1877,7 +1844,6 @@ tile(Monitor *m)
 			ty += HEIGHT(c);
 		}
 }
-#endif
 
 void
 togglebar(const Arg *arg)
