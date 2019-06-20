8641e1b31fb441462cc3abf03ed8d914f4c6cf95
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-06-19 22:13:53 -0600

    changed snap as well as added support for different gaps for certain layouts

--- config.h
+++ config.h
@@ -1,8 +1,10 @@
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int deltamv   = 20; /* deltamvmv = the amount of pixels each movement will be by */
-static const unsigned int gappx     = 5;
-static const unsigned int snap      = 60; /* snap pixel */
+static const unsigned int gappx     = 10;
+static const unsigned int gappxi    = 15; //only affects vantile
+static const unsigned int gappxo    = 10; //only affects vantile
+static const unsigned int snap      = 20; /* snap pixel */
 static const int showbar            = 1;  /* 0 means no bar */
 static const int topbar             = 1;  /* 0 means bottom bar */
 static const char *fonts[]          = { "DejaVu Sans Mono:style=Regular:size=10" };
